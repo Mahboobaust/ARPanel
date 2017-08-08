28ac4920e91e3fb9ee3931e478a0394cb54541fa
Mahboob <Mahboob@mahboobs-mac-mini.local>
2017-08-08 17:40:33 +0600

    Initial Commit

---
+++ ARPanel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARPanel
+//
+//  Created by Mahboob on 8/8/17.
+//  Copyright © 2017 Empire Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARPanel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARPanel
+//
+//  Created by Mahboob on 8/8/17.
+//  Copyright © 2017 Empire Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
